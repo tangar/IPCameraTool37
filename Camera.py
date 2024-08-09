bbf4f3652d1da2a9172c4f4b4d61b00c8e8b10d3
Pochitaev Ilia <pochitaev.ilya@gmail.com>
2024-08-10 00:45:59 +0300

    ПРодолжение разработки

--- Camera.py
+++ Camera.py
@@ -20,9 +20,11 @@ class App(QtWidgets.QMainWindow, CameraGuiNew.Ui_MainWindow):
         self.setupUi(self)
 
         self.camera: Optional[CameraController] = None
-        self.cap = None
+        self.cap_main = None
+        self.cap_second = None
 
         self.rtsp_url_main = "rtsp://admin:6m8vw@198.0.100.108:80/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif"
+        # self.rtsp_url_second = "rtsp://admin:admin@198.0.100.109:554/stream0"
         self.rtsp_url_second = "rtsp://admin:6m8vw@198.0.100.108:80/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif"
         
         self.savePath = './Frames'
@@ -42,10 +44,12 @@ class App(QtWidgets.QMainWindow, CameraGuiNew.Ui_MainWindow):
 
         self.shotButton.clicked.connect(lambda: self.make_shot())
 
-        self.cap = cv2.VideoCapture(self.rtsp_url_main)
+        self.cap_main = cv2.VideoCapture(self.rtsp_url_main)
+        self.cap_second = cv2.VideoCapture(self.rtsp_url_second)
 
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.updateFrame)
+        self.timer.timeout.connect(self.updateFrameMain)
+        self.timer.timeout.connect(self.updateFrameSec)
         self.timer.start(5)
         
         self.setPhotoPath.triggered.connect(lambda: self.configPath())
@@ -66,8 +70,8 @@ class App(QtWidgets.QMainWindow, CameraGuiNew.Ui_MainWindow):
             self.camera.connect()
             print(self.camera.zoom_level)
             print(self.camera.focus_level)
-            self.zoomSlider.setValue(self.camera.zoom_level * 100)
-            self.FocusSlider.setValue(self.camera.focus_level * 100)
+            self.zoomSlider.setValue(int(self.camera.zoom_level * 100))
+            self.FocusSlider.setValue(int(self.camera.focus_level * 100))
             self.appendText('Камера подключена')
         else:
             self.find_camera()
@@ -122,22 +126,37 @@ class App(QtWidgets.QMainWindow, CameraGuiNew.Ui_MainWindow):
             self.focusDownButton.setDisabled(False)
 
     def make_shot(self):
-        if self.cap:
-            try:
-                ret, frame = self.cap.read()
-                stamp = datetime.now()
-                datetime_str = stamp.strftime("%Y-%m-%d_%H-%M-%S")
-                full_file_name = "{} Frame.jpg".format(self.savePath + '/' + datetime_str)
-                cv2.imwrite(full_file_name, frame)
-                self.textBrowser.setText(f'Файл сохранён {full_file_name}')
+        if self.tabWidget.currentIndex == 0:
+            if self.cap_main:
+                try:
+                    ret, frame = self.cap_main.read()
+                    stamp = datetime.now()
+                    datetime_str = stamp.strftime("%Y-%m-%d_%H-%M-%S")
+                    full_file_name = "{} Frame.jpg".format(self.savePath + '/' + datetime_str)
+                    cv2.imwrite(full_file_name, frame)
+                    self.textBrowser.setText(f'Файл сохранён {full_file_name}')
 
-            except Exception:
-                self.textBrowser.setText(f'Не удалось сделать снимок. {Exception}')
+                except Exception:
+                    self.textBrowser.setText(f'Не удалось сделать снимок. 1 {Exception}')
+            else:
+                self.textBrowser.setText('Не удалось сделать снимок. Камера 1 не подключена')
         else:
-            self.textBrowser.setText('Не удалось сделать снимок. Камера не подключена')
+            if self.cap_second:
+                try:
+                    ret, frame = self.cap_second.read()
+                    stamp = datetime.now()
+                    datetime_str = stamp.strftime("%Y-%m-%d_%H-%M-%S")
+                    full_file_name = "{} Frame.jpg".format(self.savePath + '/' + datetime_str)
+                    cv2.imwrite(full_file_name, frame)
+                    self.textBrowser.setText(f'Файл сохранён {full_file_name}')
 
-    def updateFrame(self):
-        ret, frame = self.cap.read()
+                except Exception:
+                    self.textBrowser.setText(f'Не удалось сделать снимок. 2 {Exception}')
+            else:
+                self.textBrowser.setText('Не удалось сделать снимок. Камера 2 не подключена')
+
+    def updateFrameMain(self):
+        ret, frame = self.cap_main.read()
         if ret:
             # Convert the frame to a QPixmap for display
             var = self.main_cam.frameSize()
@@ -155,9 +174,29 @@ class App(QtWidgets.QMainWindow, CameraGuiNew.Ui_MainWindow):
             qPix = QPixmap.fromImage(qImg)
             self.main_cam.setPixmap(qPix)
 
+    def updateFrameSec(self):
+        ret, frame = self.cap_second.read()
+        if ret:
+            # Convert the frame to a QPixmap for display
+            var = self.second_cam.frameSize()
+            
+            #target_width, target_height = 640, 480  # Задайте нужный размер
+            target_width = var.width()
+            target_height = int(var.width() / 16 * 9)
+
+            frame = cv2.resize(frame, (target_width, target_height))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            height, width, channel = frame.shape
+            step = channel * width
+
+            qImg = QImage(frame.data, frame.shape[1], frame.shape[0], step, QImage.Format_RGB888)
+            qPix = QPixmap.fromImage(qImg)
+            self.second_cam.setPixmap(qPix)
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = App()
+    window.setWindowTitle("Управление эндоскопом Э-37")
     window.show()
     app.exec_()
 
